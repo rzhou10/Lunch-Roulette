35de6e04e917b516734c25cd1f30e99a61e26e7a
rzhou10 <rzhou10@gmail.com>
2019-02-06 21:04:44 -0500

    Updates

---
+++ precache-manifest.ffb8c07440518425881ac7fe6d6aacc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4c181bf9d355db1889b9",
+    "url": "/lunchRoulette/static/css/main.67ef7ecc.chunk.css"
+  },
+  {
+    "revision": "4c181bf9d355db1889b9",
+    "url": "/lunchRoulette/static/js/main.4c181bf9.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "/lunchRoulette/static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "b39d927d5de35c1b45ca",
+    "url": "/lunchRoulette/static/js/runtime~main.b39d927d.js"
+  },
+  {
+    "revision": "40c1c7452344f87c86a45097337e806d",
+    "url": "/lunchRoulette/index.html"
+  }
+];
